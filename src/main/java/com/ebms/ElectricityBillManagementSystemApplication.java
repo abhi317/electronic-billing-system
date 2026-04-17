ef7efddd07596e0c44ae851710d399cb997ca3aa
abhi317 <abhi31723@gmail.com>
2026-04-17 15:55:03 +0530

    app first commit

---
+++ src/main/java/com/ebms/ElectricityBillManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ebms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBillManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBillManagementSystemApplication.class, args);
+	}
+
+}
