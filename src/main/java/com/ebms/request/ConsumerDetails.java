061e821c7cf9fdb6ead5c5665c70ec7d78e8b8b6
abhi317 <abhi31723@gmail.com>
2026-04-17 16:02:22 +0530

    Request class added

---
+++ src/main/java/com/ebms/request/ConsumerDetails.java
@@ -0,0 +1,20 @@
+package com.ebms.request;
+
+public class ConsumerDetails {
+
+	private Integer consumerId;
+	private String consumeName;
+	
+	public Integer getConsumerId() {
+		return consumerId;
+	}
+	public void setConsumerId(Integer consumerId) {
+		this.consumerId = consumerId;
+	}
+	public String getConsumeName() {
+		return consumeName;
+	}
+	public void setConsumeName(String consumeName) {
+		this.consumeName = consumeName;
+	}
+}
